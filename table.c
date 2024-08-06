29815ae72b639dc63fff5bb1ed85607aeab09513
sameer shukla <anshu87653869>
2024-08-06 23:58:00 +0530

    doremon

---
+++ table.c
@@ -0,0 +1,39 @@
+/*#include<stdio.h>
+int main()
+{
+    int a,b; 
+    printf("Enter the number a\n");
+    scanf("%d",&a);
+    printf("enter the number  b\n");
+    scanf("%d",&b);
+    printf("the sum of number is %d\n", a+b);
+
+    return 0;*/
+/*#include<stdio.h>
+int main(){
+int a,b;
+a = 21;
+b = 12;
+printf("a+b=%d\n",a+b);
+printf("a-b=%d\n",a-b);
+printf("a*b=%d\n",a*b);
+printf("a/b=%d\n",a/b);
+return 0;
+}
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a = 17;
+    b = 15;
+    printf("a!=b=%d\n",a!=b);
+    printf("a>b=%d\n",a>b);
+    printf("a<b=%d\n",a<b);
+
+    
+}*/
+#include<stdio.h>
+int main()
+{
+    
+}
